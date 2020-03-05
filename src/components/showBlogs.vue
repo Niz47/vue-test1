00795110266aa62b6a076e2c65470de97be50269
Niz47 <zinmarhtun47@gmail.com>
2020-03-05 00:58:31 -0800

    retrieve all blogs and single blog from firbase realtime db

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -4,7 +4,10 @@
     <input type="text" v-model="search" placeholder="search blogs"/>
     <div v-for="blog in filteredBlogs" class="single-blog">
       <router-link v-bind:to="'/blog/' + blog.id"><h2 v-rainbow>{{blog.title | toUppercase}}</h2></router-link>
-      <article>{{blog.body | snippet}}</article>
+      <article>{{blog.content | snippet}}</article>
+
+      <!-- <router-link v-bind:to="'/blog/' + blog.id"><h2 v-rainbow>{{blog.title | toUppercase}}</h2></router-link>
+      <article>{{blog.body | snippet}}</article> -->
     </div>
   </div>
 </template>
@@ -22,12 +25,28 @@ export default {
   methods: {
     
   },
+  
   created(){
-    this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
-      // console.log(data);
-      this.blogs = data.body.slice(0,10);
+    this.$http.get('https://vue-test1-8c174.firebaseio.com/posts.json').then(function(data){
+      return data.json();
+    }).then(function(data){
+      var blogsArray =[];
+      for (let key in data){
+        data[key].id = key;
+        blogsArray.push(data[key]);
+      }
+      console.log(blogsArray);
+      this.blogs = blogsArray;
     })
   },
+
+  // JsonPlaceholder - fake resource
+  // created(){
+  //   this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
+  //     // console.log(data);
+  //     this.blogs = data.body.slice(0,10);
+  //   })
+  // },
   
   filters: {
     // 'to-uppercase':function(value){
@@ -64,5 +83,7 @@ export default {
   box-sizing: border-box;
   background: #eee;
 }
-
+a {
+  text-decoration: none;
+}
 </style>
--- src/components/singleBlog.vue
+++ src/components/singleBlog.vue
@@ -1,7 +1,14 @@
 <template>
     <div id="single-blog">
         <h1>{{blog.title}}</h1>
-        <article>{{blog.body}}</article>
+        <article>{{blog.content}}</article>
+
+        <!-- <article>{{blog.body}}</article> -->
+
+        <p>Author: {{blog.author}}</p>
+        <ul>
+            <li v-for="category in blog.categories">{{category}}</li>
+        </ul>
     </div>
 </template>
 
@@ -14,9 +21,15 @@ export default {
         }
     },
     created(){
-        this.$http.get("https://jsonplaceholder.typicode.com/posts/"+this.id).then(function(data){
-            // console.log(data);
-            this.blog = data.body;
+        // this.$http.get("https://jsonplaceholder.typicode.com/posts/"+this.id).then(function(data){
+        //     console.log(data);
+        //     this.blog = data.body;
+        // });
+        this.$http.get("https://vue-test1-8c174.firebaseio.com/posts/"+this.id+".json").then(function(data){
+            console.log(data);
+            return data.json();
+        }).then(function(data){
+            this.blog = data;
         });
     }
 }
@@ -24,6 +37,6 @@ export default {
 
 <style scoped>
 #single-blog{
-    padding: 10px 30px;
+    padding: 10px 80px;
 }
 </style>
